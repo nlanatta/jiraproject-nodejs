2ce8fec10d1bea288029d3d247270968b44dcf39
nlanatta <Nicolas.Lanatta@web.com>
2018-01-26 16:32:31 -0300

    Call jira from client

--- routes/jiraController.js
+++ routes/jiraController.js
@@ -5,11 +5,11 @@ import Session from './session';
 const router = express.Router();
 
 
-router.get('/issue/:issueKey', function(req, res, next) {
-    console.log(req.session);
-    var Request = unirest.get('https://jira.registeredsite.com/rest/api/2/issue/'+req.params.issueKey);
+router.get('/issue/:issue/:cookie', function(req, res, next) {
+    var Request = unirest.get('https://jira.registeredsite.com/rest/api/2/issue/'+req.params.issue);
       Request.headers({'Content-Type': 'application/json',
-                        'cookie': req.session.sessionName+"="+req.session.sessionValue});
+                        'cookie': req.params.cookie//req.session.sessionName+"="+req.session.sessionValue
+                      });
       Request.end(function (response) {
         console.log(response.body);
         res.json(response.body);
